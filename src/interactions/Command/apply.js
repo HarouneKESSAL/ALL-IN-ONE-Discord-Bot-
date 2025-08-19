870a0d3401e7f11c4d0d673133905093049b61a9
Haroune Kessal <76233588+HarouneKESSAL@users.noreply.github.com>
2025-08-19 18:19:51 +0100

    Update apply.js

--- src/interactions/Command/apply.js
+++ src/interactions/Command/apply.js
@@ -16,7 +16,6 @@ module.exports = {
                 .addStringOption(option => option.setName('roles').setDescription('Roles users can apply for').setRequired(true))
                 .addChannelOption(option => option.setName('log').setDescription('The channel for the application logs').setRequired(true).addChannelTypes(ChannelType.GuildText))
                 .addStringOption(option => option.setName('gif').setDescription('GIF URL to display in the embed'))
-
         ),
 
     /**
