02d7eb28e5c259d38158cc08262828ad1a64efac
Haroune Kessal <76233588+HarouneKESSAL@users.noreply.github.com>
2025-08-19 18:15:19 +0100

    Update verifyV2.js

--- src/database/models/verifyV2.js
+++ src/database/models/verifyV2.js
@@ -5,9 +5,7 @@ const Schema = new mongoose.Schema({
     Channel: String,
     Role: String,
     LogChannel: String,
-
     AccessRole: String,
-
 });
 
 module.exports = mongoose.model('verifyV2', Schema);
